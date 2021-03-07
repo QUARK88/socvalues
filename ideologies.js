09120e1eb137694d8a886a1eaf41afa7ac7e0a81
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-03-07 08:09:19 -0500

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,21 @@
+ideologies = [
+    {
+        "name": "Centrism",
+        "stats": {
+            "becon": 50,
+            "blead": 50,
+            "bindu": 50,
+            "bscie": 50,
+            "bstat": 50,
+            "bsoci": 50,
+            "brevi": 50,
+            "bnati": 50,
+            "btrad": 50,
+            "bimpl": 50,
+            "bpoli": 50,
+            "barmy": 50,
+            "binte": 50,
+            "belit": 50
+        }
+    }
+];
