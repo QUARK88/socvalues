0af07a4fe56d3504264ed8106f236e46dde37ac2
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-03-07 18:42:43 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,20 +2,20 @@ questions = [
     {
         "question": ".",
         "effect": {
-            "becon": 0,
-            "blead": 0,
-            "bindu": 0,
-            "bscie": 0,
-            "bstat": 0,
-            "bsoci": 0,
-            "brevi": 0,
-            "bnati": 0,
-            "btrad": 0,
-            "bimpl": 0,
-            "bpoli": 0,
-            "barmy": 0,
-            "binte": 0,
-            "belit": 0
+            "becon": 10,
+            "blead": 10,
+            "bindu": 10,
+            "bscie": 10,
+            "bstat": 10,
+            "bsoci": 10,
+            "brevi": 10,
+            "bnati": 10,
+            "btrad": 10,
+            "bimpl": 10,
+            "bpoli": 10,
+            "barmy": 10,
+            "binte": 10,
+            "belit": 10
         }
     },
     {
