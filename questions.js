35f105179c9c7ae3b55c69754781e735e4e5f2a5
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-03-07 08:08:32 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,40 @@
+questions = [
+    {
+        "question": ".",
+        "effect": {
+            "becon": 0,
+            "blead": 0,
+            "bindu": 0,
+            "bscie": 0,
+            "bstat": 0,
+            "bsoci": 0,
+            "brevi": 0,
+            "bnati": 0,
+            "btrad": 0,
+            "bimpl": 0,
+            "bpoli": 0,
+            "barmy": 0,
+            "binte": 0,
+            "belit": 0
+        }
+    }
+    {
+        "question": ".",
+        "effect": {
+            "becon": 0,
+            "blead": 0,
+            "bindu": 0,
+            "bscie": 0,
+            "bstat": 0,
+            "bsoci": 0,
+            "brevi": 0,
+            "bnati": 0,
+            "btrad": 0,
+            "bimpl": 0,
+            "bpoli": 0,
+            "barmy": 0,
+            "binte": 0,
+            "belit": 0
+        }
+    },
+];
