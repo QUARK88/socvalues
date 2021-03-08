277e1c8492499609ecfe2edebf3511f74f73a10f
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-03-08 08:03:19 -0500

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -17,5 +17,138 @@ ideologies = [
             "binte": 50,
             "belit": 50
         }
+    },
+    {
+        "name": "Stalinism",
+        "stats": {
+            "becon": 100,
+            "blead": 100,
+            "bindu": 0,
+            "bscie": 50,
+            "bstat": 100,
+            "bsoci": 50,
+            "brevi": 65,
+            "bnati": 0,
+            "btrad": 10,
+            "bimpl": 100,
+            "bpoli": 100,
+            "barmy": 100,
+            "binte": 80,
+            "belit": 75
+        }
+    },
+    {
+        "name": "Trotskyism",
+        "stats": {
+            "becon": 90,
+            "blead": 80,
+            "bindu": 10,
+            "bscie": 65,
+            "bstat": 70,
+            "bsoci": 70,
+            "brevi": 70,
+            "bnati": 100,
+            "btrad": 25,
+            "bimpl": 75,
+            "bpoli": 75,
+            "barmy": 75,
+            "binte": 75,
+            "belit": 25
+        }
+    },
+    {
+        "name": "Socialist Nationalism",
+        "stats": {
+            "becon": 50,
+            "blead": 100,
+            "bindu": 90,
+            "bscie": 90,
+            "bstat": 50,
+            "bsoci": 50,
+            "brevi": 50.0,
+            "bnati": 0,
+            "btrad": 50.0,
+            "bimpl": 50.0,
+            "bpoli": 15,
+            "barmy": 50.0,
+            "binte": 50.0,
+            "belit": 50
+        }
+    },
+    {
+        "name": "Anarcho Communism",
+        "stats": {
+            "becon": 100,
+            "blead": 50,
+            "bindu": 50,
+            "bscie": 75,
+            "bstat": 0,
+            "bsoci": 100,
+            "brevi": 25,
+            "bnati": 100,
+            "btrad": 50,
+            "bimpl": 100,
+            "bpoli": 0,
+            "barmy": 50,
+            "binte": 50,
+            "belit": 0
+        }
+    },
+    {
+        "name": "Anarcho Syndicalism",
+        "stats": {
+            "becon": 50,
+            "blead": 0,
+            "bindu": 25,
+            "bscie": 100,
+            "bstat": 0,
+            "bsoci": 60,
+            "brevi": 0,
+            "bnati": 100,
+            "btrad": 75,
+            "bimpl": 65,
+            "bpoli": 0,
+            "barmy": 0,
+            "binte": 25,
+            "belit": 0
+        }
+    },
+    {
+        "name": "Capacitationism",
+        "stats": {
+            "becon": 0,
+            "blead": 0,
+            "bindu": 0,
+            "bscie": 100,
+            "bstat": 0,
+            "bsoci": 100,
+            "brevi": 50,
+            "bnati": 100,
+            "btrad": 100,
+            "bimpl": 25,
+            "bpoli": 40,
+            "barmy": 100,
+            "binte": 100,
+            "belit": 75
+        }
+    },
+    {
+        "name": "Juche",
+        "stats": {
+            "becon": 100,
+            "blead": 100,
+            "bindu": 0,
+            "bscie": 100,
+            "bstat": 100,
+            "bsoci": 100,
+            "brevi": 75,
+            "bnati": 0,
+            "btrad": 0,
+            "bimpl": 100,
+            "bpoli": 100,
+            "barmy": 100,
+            "binte": 0,
+            "belit": 100
+        }
     }
 ];
